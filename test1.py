7073bbd7dad700ab4c41454d02a8f73078bd272c
unknown <llukeandersonn@gmail.com>
2025-01-21 10:45:56 -0700

    initial OCR attempts

---
+++ test1.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from PIL import Image
+ 
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img = cv2.imread('Apex_Scoreboard.png', cv2.IMREAD_GRAYSCALE)
+# Apply Otsu's thresholding
+ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Display the image
+cv2.imshow('Otsu Thresholding', thresh)
+print(pytesseract.image_to_string(thresh))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
