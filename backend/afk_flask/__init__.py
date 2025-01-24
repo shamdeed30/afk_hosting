684f5953d5ecde0656e5ea6e85d92f935a1d5e59
primerahour <hourprimera@gmail.com>
2025-01-24 12:20:15 -0700

    changed ip address

--- backend/afk_flask/__init__.py
+++ backend/afk_flask/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = '40.85.147.30'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'SCAC_STATS'
