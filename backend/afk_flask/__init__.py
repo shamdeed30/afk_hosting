527151cd5dcc6df5310a4e4c3e490d67a8907f30
primerahour <hourprimera@gmail.com>
2025-01-24 13:33:08 -0700

    changed user

--- backend/afk_flask/__init__.py
+++ backend/afk_flask/__init__.py
@@ -10,7 +10,7 @@ CORS(app)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'afkuser'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'afk'
 app.config['MYSQL_DB'] = 'SCAC_STATS'
 
 # Database connection
