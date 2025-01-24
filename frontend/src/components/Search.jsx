9a42ef28584715724da12f992789277d707d6152
primerahour <hourprimera@gmail.com>
2025-01-24 11:51:42 -0700

    changed port again

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -18,7 +18,7 @@ const Search = () => {
   const getGameReports = async () => {
     try {
       const response = await fetch(
-        `http://localhost:8080/stats/${game}/${week}`,
+        `http://40.85.147.30:8080/stats/${game}/${week}`,
       );
 
       if (response.ok) {
