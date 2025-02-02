85bb21b50ae83ddb3808a037fe4bf367542bf707
shamdeed30 <117672677+shamdeed30@users.noreply.github.com>
2025-02-02 14:53:10 -0700

    updated

--- backend/ocr/Apex/ApexFuncs.py
+++ backend/ocr/Apex/ApexFuncs.py
@@ -32,54 +32,54 @@ def apex_OCR(img_file):
 
 
 # get kills/assists/knocks
-# def get_KAK_simple(img_file):
-#     img = cv2.imread(img_file)
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     _, bin = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+def get_KAK_simple(img_file):
+    img = cv2.imread(img_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, bin = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-#     cv2.imwrite('init_image.jpg', bin)
+    cv2.imwrite('init_image.jpg', bin)
 
-#     d = pytesseract.image_to_data(gray, output_type=Output.DICT, lang='eng', config='--psm 6 -c tessedit_char_whitelist="0123456789/"') #abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+    d = pytesseract.image_to_data(gray, output_type=Output.DICT, lang='eng', config='--psm 6 -c tessedit_char_whitelist="0123456789/"') #abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 
 
-#     box_img = img.copy()
+    box_img = img.copy()
 
-#     # detected boxes
-#     n_boxes = len(d['text'])
+    # detected boxes
+    n_boxes = len(d['text'])
 
-#     all_text = []
-#     for i in range(n_boxes):
-#         if int(d['conf'][i]) > 10:  # Adjust confidence threshold
-#             (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+    all_text = []
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 10:  # Adjust confidence threshold
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
             
-#             # expand
-#             x = max(x - 5 - 5, 0)
-#             y = max(y - 5 - 5, 0)
-#             w = w + 10 + 10
-#             h = h + 10 + 10
+            # expand
+            x = max(x - 5 - 5, 0)
+            y = max(y - 5 - 5, 0)
+            w = w + 10 + 10
+            h = h + 10 + 10
 
-#             # rectangle
-#             box_img = cv2.rectangle(box_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # rectangle
+            box_img = cv2.rectangle(box_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-#             # crop
-#             crop = img[y:y + h, x:x + w]
-#             crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+            # crop
+            crop = img[y:y + h, x:x + w]
+            crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
         
-#             _, crop = cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
-#             crop = scale_img(crop, 150)
+            _, crop = cv2.threshold(crop, 127, 255, cv2.THRESH_BINARY)
+            crop = scale_img(crop, 150)
 
-#             text = pytesseract.image_to_string(crop, config='--psm 6 -c tessedit_char_whitelist="0123456789/"')
-#             print(f"Detected text in box {i}: {text.strip()}")
-#             if i == 17 or i == 18 or i == 19:
-#                 cv2.imwrite(f'KAK_crops_simple/crop_sample{i}.png', crop)
+            text = pytesseract.image_to_string(crop, config='--psm 6 -c tessedit_char_whitelist="0123456789/"')
+            print(f"Detected text in box {i}: {text.strip()}")
+            if i == 17 or i == 18 or i == 19:
+                cv2.imwrite(f'KAK_crops_simple/crop_sample{i}.png', crop)
             
-#             all_text.append(text[:-1])
+            all_text.append(text[:-1])
 
-#     cv2.imwrite('processed_boxed_image.jpg', box_img)
-#     return list(filter(None, all_text))
+    cv2.imwrite('processed_boxed_image.jpg', box_img)
+    return list(filter(None, all_text))
 
-# def get_KAK(img_file, pn):
+def get_KAK(img_file, pn):
     files = glob.glob('KAK_crops_temp/*')
     for f in files:
         os.remove(f)
